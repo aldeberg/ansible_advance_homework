634364b644c4be4f2d0cf3f58f25fac992eaa63c
PRASANNA HOLLA <prasanna.holla@in.ibm.com>
2019-10-11 14:09:10 +0530

    cleanup

---
+++ environment_setup.sh
@@ -0,0 +1,12 @@
+TOWER_GUID=
+OSP_GUID=
+OPENTLC_LOGIN=
+OPENTLC_PASSWORD=
+GITHUB_REPO=https://github.com/prasanna-holla/ansible_advance_homework
+JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
+REGION=us-east-1
+RH_MAIL_ID=prasanna.holla@in.ibm.com
+
+ansible-playbook main_playbook.yml -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=/root/.ssh/openstack.pem -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
+
+#ansible-playbook site-config-tower.yml -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=/root/.ssh/openstack.pem -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
